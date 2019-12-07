8a040d5d79fbae049d01f87b96e6ba69595806e4
Vincent Cheung <vfhcheung@gmail.com>
2019-12-06 22:31:56 -0800

    Implement resetting and updating password

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SignInForm from "./SignInForm";
 import { SignUpLink } from "../SignUp";
+import { PasswordForgetLink } from "../PasswordForget";
 
 export default function SignInPage() {
   return (
     <div>
       <h1>SignIn</h1>
       <SignInForm />
+      <PasswordForgetLink />
       <SignUpLink />
     </div>
   );
