8740174ea94256644c48692d061e970d06437e5c
Vincent Cheung <vfhcheung@gmail.com>
2019-12-17 20:05:12 -0800

    Use pregenerated ids instead of custom ids

--- src/components/Resources/Resource/ResourcesList.js
+++ src/components/Resources/Resource/ResourcesList.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { DocumentDeleteButton } from '../../Firebase';
 
-export default function ResourcesList({ resources, category }) {
+export default function ResourcesList({ resources, categoryURLId, categoryFirestoreId }) {
   const resourceItems = resources.map((resource) => (
     <tr key={resource.id}>
       <td>{resource.id}</td>
       <td>{resource.title}</td>
       <td>{resource.description}</td>
-      <td><Link to={`${category}/${resource.id}`}>View resource</Link></td>
-      <td><DocumentDeleteButton path={`resource_categories/${category}/resources/${resource.id}`} /></td>
+      <td><Link to={`${categoryURLId}/${resource.urlId}`}>View resource</Link></td>
+      <td><DocumentDeleteButton path={`resource_categories/${categoryFirestoreId}/resources/${resource.id}`} /></td>
     </tr>
   ));
 
@@ -37,5 +37,10 @@ ResourcesList.propTypes = {
       description: PropTypes.string.isRequired,
     }),
   ).isRequired,
-  category: PropTypes.string.isRequired,
+  categoryURLId: PropTypes.string.isRequired,
+  categoryFirestoreId: PropTypes.string,
+};
+
+ResourcesList.defaultProps = {
+  categoryFirestoreId: null,
 };
--- src/components/Resources/Resource/index.js
+++ src/components/Resources/Resource/index.js
@@ -1,29 +1,74 @@
-import { createEditorStateFromRaw } from 'draftail';
-import React from 'react';
-import { useParams } from 'react-router-dom';
 import { Editor } from 'draft-js';
-import { useDocumentSnapshot } from '../../../hooks';
+import { createEditorStateFromRaw } from 'draftail';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { withAuthorization } from '../../Session';
 import NewResourceForm from './NewResourceForm';
-import ResourcesList from './ResourcesList';
 import NewResourcePage from './NewResourcePage';
+import ResourcesList from './ResourcesList';
 
 function ResourcePage() {
-  const { category, resourceId } = useParams();
-  const { data, loading, error } = useDocumentSnapshot(`resource_categories/${category}/resources/${resourceId}`);
+  const { categoryURLId, resourceURLId } = useParams();
+
+  const [categoryDoc, setCategoryDoc] = useState({});
+  const [categoryLoading, setCategoryLoading] = useState(true);
+  const [categoryError, setCategoryError] = useState(null);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('resource_categories')
+      .where('urlId', '==', categoryURLId)
+      .get()
+      .then((snapshot) => {
+        setCategoryLoading(false);
+        snapshot.docs.forEach((doc) => {
+          setCategoryDoc({ ...doc.data(), id: doc.id });
+        });
+      })
+      .catch((err) => {
+        setCategoryError(err);
+      });
+  }, [categoryURLId]);
+
+  const [resourceDoc, setResourceDoc] = useState({});
+  const [resourceLoading, setResourceLoading] = useState(true);
+  const [resourceError, setResourceError] = useState(null);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(`resource_categories/${categoryDoc.id}/resources`)
+      .where('urlId', '==', resourceURLId || '')
+      .get()
+      .then((snapshot) => {
+        setResourceLoading(false);
+        snapshot.docs.forEach((doc) => {
+          setResourceDoc({ ...doc.data(), id: doc.id });
+        });
+      })
+      .catch((err) => {
+        setResourceError(err);
+      });
+  }, [categoryDoc.id, resourceURLId]);
 
   return (
     <div>
-      {loading && <p>loading...</p>}
-      {error && <p>{error.message}</p>}
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
+
+      {!categoryLoading && <h1>{categoryDoc.title}</h1>}
+      {categoryError && <p>{categoryError.message}</p>}
+      <h1>{resourceDoc.title}</h1>
+      <p>{resourceDoc.description}</p>
+      {resourceLoading && <p>loading...</p>}
+      {resourceError && <p>{resourceError.message}</p>}
 
       <hr />
 
       <div>
-        {data.body
-        && <Editor readOnly editorState={createEditorStateFromRaw(JSON.parse(data.body))} />}
+        {resourceDoc.body
+        && <Editor readOnly editorState={createEditorStateFromRaw(JSON.parse(resourceDoc.body))} />}
       </div>
     </div>
   );
