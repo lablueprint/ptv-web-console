acdb7f41745541054eb24d612ee2105446ace9ec
Vincent Cheung <vfhcheung@gmail.com>
2019-12-19 02:21:50 -0800

    Refactor NewResourcePage

--- src/components/Resources/Resource/NewResourcePage.js
+++ src/components/Resources/Resource/NewResourcePage.js
@@ -2,15 +2,14 @@ import 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as firebase from 'firebase/app';
+import ClipLoader from 'react-spinners/ClipLoader';
 import { withAuthorization } from '../../Session';
 import NewResourceForm from './NewResourceForm';
 
-function NewResourcePage() {
-  const { categoryURLId } = useParams();
-
-  const [categoryDoc, setCategoryDoc] = useState({});
-  const [categoryLoading, setCategoryLoading] = React.useState(true);
-  const [categoryError, setCategoryError] = useState(null);
+function useCategoryFromURLId(categoryURLId, isMounted) {
+  const [data, setCategoryDoc] = useState({});
+  const [loading, setCategoryLoading] = React.useState(true);
+  const [error, setCategoryError] = useState(null);
 
   useEffect(() => {
     firebase
@@ -19,22 +18,45 @@ function NewResourcePage() {
       .where('urlId', '==', categoryURLId)
       .get()
       .then((snapshot) => {
-        setCategoryLoading(false);
-        snapshot.docs.forEach((doc) => {
-          setCategoryDoc({ ...doc.data(), id: doc.id });
-        });
+        if (isMounted) {
+          setCategoryLoading(false);
+          snapshot.docs.forEach((doc) => {
+            setCategoryDoc({ ...doc.data(), id: doc.id });
+          });
+        }
       })
       .catch((err) => {
-        setCategoryError(err);
+        if (isMounted) {
+          setCategoryError(err);
+        }
       });
-  }, [categoryURLId]);
+  }, [categoryURLId, isMounted]);
 
+  return { data, loading, error };
+}
+
+function NewResourcePage() {
+  const { categoryURLId } = useParams();
+  const [isMounted, setIsMounted] = useState(false);
+  const { data, loading, error } = useCategoryFromURLId(categoryURLId, isMounted);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>{!categoryLoading && `Create a new resource in ${categoryDoc.title} category`}</h1>
-      {categoryError && <p>{categoryError.message}</p>}
-      <NewResourceForm categoryURLId={categoryURLId} categoryFirestoreId={categoryDoc.id} />
+      <ClipLoader loading={loading} />
+      {!loading && (
+        <>
+          <h1>{`Create a new resource in ${data.title} category`}</h1>
+          {error && <p>{error.message}</p>}
+          <NewResourceForm categoryURLId={categoryURLId} categoryFirestoreId={data.id} />
+        </>
+      )}
     </div>
   );
 }
