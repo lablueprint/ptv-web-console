a702218a8706bd283d8d5f12fdce9f6dceff7fe6
Vincent Cheung <vfhcheung@gmail.com>
2020-01-01 20:00:46 -0800

    Edit toolbar

--- src/components/Resources/Resource/toolbarOptions.js
+++ src/components/Resources/Resource/toolbarOptions.js
@@ -1,19 +1,15 @@
 export default [
-  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-  ['blockquote', 'code-block'],
-
-  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  ['bold', 'italic', 'underline'],
   [{ list: 'ordered' }, { list: 'bullet' }],
-  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+  [{ align: [] }],
   [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
   [{ direction: 'rtl' }], // text direction
-
-  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  ['link', 'image', 'video', 'formula'], // add's image support
+  ['link', 'image'], // add's image support
   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-  [{ font: [] }],
-  [{ align: [] }],
-
+  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+  ['blockquote'],
   ['clean'], // remove formatting button
 ];
