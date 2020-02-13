40a6f0a0b4dd0b213af7820aa55ce800ec71bb75
Vincent Cheung <vfhcheung@gmail.com>
2020-02-13 15:50:13 -0800

    Add Category Grid with thumbnails

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // we don't care that .js files contain JSX code
     'react/jsx-props-no-spreading': 'off', // HOCs should have prop spreading
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "no-use-before-define": ["error", { "functions": true, "classes": true, "variables": false }], // disable the rule for variables, but enable it for functions and classes
   },
 };
