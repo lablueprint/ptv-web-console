7bab542e8523e830ef3267d98f4c37413c6795b8
Bill Liu <bill.liu.school@gmail.com>
2020-04-20 22:19:50 -0400

    Added search user filtering feature

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,46 +8,93 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
+  searchBarRoot: {
+    marginBottom: 20,
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '20%',
+  },
+  input: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
 });
 
-export default function UsersList({ users }) {
-  const classes = useStyles();
-  const userItems = users.map((user) => (
+function GetUserItems({ users, searchText }) {
+  return users.filter((user) => !searchText
+  || ((user.name ? user.name : '').toLowerCase().search(searchText) !== -1
+  || (user.displayName ? user.displayName : '').toLowerCase().search(searchText) !== -1)
+  || (user.email ? user.email : '').toLowerCase().search(searchText) !== -1).map((user) => (
     <TableRow key={user.id}>
-      <TableCell component="th" scope="row">{user.id}</TableCell>
-      <TableCell align="right">{user.updatedAt ? user.updatedAt.toDate().toUTCString() : 'N/A'}</TableCell>
-      <TableCell align="right">{user.email}</TableCell>
-      <TableCell align="right">{user.role}</TableCell>
-      <TableCell align="right">
+      <TableCell component="th" scope="row">{user.displayName}</TableCell>
+      <TableCell align="center">{user.name}</TableCell>
+      <TableCell align="center">{user.updatedAt ? user.updatedAt.toDate().toUTCString() : 'N/A'}</TableCell>
+      <TableCell align="center">{user.email}</TableCell>
+      <TableCell align="center">{user.role}</TableCell>
+      <TableCell align="center">
         <Button>
           Ban
         </Button>
       </TableCell>
     </TableRow>
   ));
+}
+
+export default function UsersList({ users }) {
+  const classes = useStyles();
+  const [userItems, setUserItems] = useState(null);
+  const [searchText, setSearchText] = useState(null);
+
+  useEffect(() => {
+    setUserItems(<GetUserItems users={users} searchText={searchText} />);
+  }, [users, searchText]);
+
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>User</TableCell>
-            <TableCell align="right">Date Joined</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Role</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {userItems}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <Paper component="form" className={classes.searchBarRoot}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search Users"
+          inputProps={{ 'aria-label': 'search users' }}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Display Name</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Date Joined</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Role</TableCell>
+              <TableCell align="center">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userItems}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
 
@@ -57,6 +104,23 @@ UsersList.propTypes = {
       id: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
       role: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      displayName: PropTypes.string.isRequired,
+      isAdmin: PropTypes.bool.isRequired,
     }),
   ).isRequired,
+};
+
+GetUserItems.propTypes = {
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      role: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      displayName: PropTypes.string.isRequired,
+      isAdmin: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  searchText: PropTypes.string.isRequired,
 };
