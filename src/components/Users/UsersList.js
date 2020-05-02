7e15b875494c943bd58a8812a1dbd33c10b4cc15
Bill Liu <bill.liu.school@gmail.com>
2020-05-01 23:28:10 -0400

    Splitting up components

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,18 +11,17 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import Switch from '@material-ui/core/Switch';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
+  },
+  normalRow: {
+    backgroundColor: 'White',
+  },
+  bannedRow: {
+    backgroundColor: 'LightCoral',
   },
   roleList: {
     display: 'inline-block',
@@ -37,184 +36,90 @@ const useStyles = makeStyles({
     textAlign: 'center',
     whiteSpace: 'nowrap',
   },
-  switchRoot: {
-    marginBottom: 20,
-    flexDirection: 'row',
-    display: 'flex',
-  },
-  switchAdmin: {
-    flex: 1,
-  },
-  dropdown: {
-    flex: 1,
-  },
-  spacing: {
-    flex: 3,
-  },
-  input: {
-    marginLeft: 10,
-    flex: 1,
-  },
-  normalRow: {
-
-  },
-  bannedRow: {
-    backgroundColor: 'LightCoral',
-  },
 });
 
-const sortOptions = [
-  'Newest Users',
-  'Oldest Users',
-  'Name - Alphabetical',
+const tableHeaders = [
+  'Full Name',
+  'Display Name',
+  'Date Joined',
+  'Email',
+  'Role',
+  'Action',
 ];
 
-export default function UsersList({ users, searchText }) {
+export default function UsersList({
+  users, searchText, adminSwitch, selectedIndex,
+}) {
   const classes = useStyles();
-  const [userItems, setUserItems] = useState(null);
-  const [adminSwitch, setAdminSwitch] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-
-  /* sort dropdown hooks */
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  /* sort dropdown functions */
-  const handleClickListItem = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-
-  const handleMenuItemClick = (_, i) => {
-    setSelectedIndex(i);
-    setAnchorEl(null);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  useEffect(() => {
-    setUserItems(
-      users.filter((user) => !searchText
-    || ((user.name ? user.name : '').toLowerCase().search(searchText) !== -1
-    || (user.displayName ? user.displayName : '').toLowerCase().search(searchText) !== -1)
-    || (user.email ? user.email : '').toLowerCase().search(searchText) !== -1)
-        .filter((user) => (adminSwitch ? user.isAdmin : !user.isAdmin))
-        .sort((userA, userB) => {
-          if (!userA.updatedAt || !userB.updatedAt) {
-            return true;
-          }
-          switch (selectedIndex) {
-          /* Newest users */
-            case 0:
-              if (userA.updatedAt.toDate() < userB.updatedAt.toDate()) {
-                return 1;
-              }
-              return -1;
-              /* Oldest users */
-            case 1:
-              if (userA.updatedAt.toDate() > userB.updatedAt.toDate()) {
-                return 1;
-              }
-              return -1;
-              /* Name - Alphabetical */
-            case 2:
-              if (userA.name > userB.name) {
-                return 1;
-              }
-              return -1;
-              /* Invalid selection */
-            default:
-              return true;
-          }
-        })
-        .map((user) => (
-          <TableRow key={user.id} className={user.isBanned ? classes.bannedRow : classes.normalRow}>
-            <TableCell component="th" scope="row">{user.displayName}</TableCell>
-            <TableCell align="center">{user.name}</TableCell>
-            <TableCell align="center">{user.updatedAt ? user.updatedAt.toDate().toUTCString() : 'N/A'}</TableCell>
-            <TableCell align="center">{user.email}</TableCell>
-            <TableCell align="center">
-              {user.role && Array.from(user.role).map((role) => (
-                <Typography component="div" className={classes.roleList}>
-                  <Typography componet="div" className={classes.roleListItem}>
-                    {role.toString()}
-                  </Typography>
-                </Typography>
-              ))}
-            </TableCell>
-            <TableCell align="center">
-              <Button onClick={() => {
-                firebase.firestore().collection('users').doc(user.id).set({
-                  ...user,
-                  isBanned: !user.isBanned,
-                })
-                  .catch((error) => {
-                    setErrorMessage(error.message);
-                  });
-              }}
-              >
-                {user.isBanned ? 'Unban' : 'Ban'}
-              </Button>
-            </TableCell>
-          </TableRow>
-        )),
-    );
-  }, [users, searchText, adminSwitch, classes, selectedIndex]);
 
   return (
     <Typography component="div">
-      <Typography component="div" className={classes.switchRoot}>
-        <Grid component="label" container alignItems="center" spacing={1} className={classes.switchAdmin}>
-          <Grid item>Standard Users</Grid>
-          <Grid item>
-            <Switch checked={adminSwitch} onChange={(e) => { setAdminSwitch(e.target.checked); }} />
-          </Grid>
-          <Grid item>Admin Users</Grid>
-        </Grid>
-        <Typography component="div" className={classes.spacing} />
-        <List component="nav" className={classes.dropdown}>
-          <ListItem
-            button
-            aria-haspopup="true"
-            aria-controls="lock-menu"
-            onClick={handleClickListItem}
-          >
-            <ListItemText primary="Sort By: " secondary={sortOptions[selectedIndex]} />
-          </ListItem>
-        </List>
-        <Menu
-          id="lock-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          {sortOptions.map((option, index) => (
-            <MenuItem
-              key={option}
-              selected={index === selectedIndex}
-              onClick={(e) => handleMenuItemClick(e, index)}
-            >
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
-      </Typography>
       <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>Display Name</TableCell>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Date Joined</TableCell>
-              <TableCell align="center">Email</TableCell>
-              <TableCell align="center">Role</TableCell>
-              <TableCell align="center">Action</TableCell>
+              {tableHeaders.map((name) => (
+                <TableCell align="center">{name}</TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {userItems}
+            {users.filter((user) => !searchText
+                          || ((user.name ? user.name : '').toLowerCase().search(searchText) !== -1
+                          || (user.displayName ? user.displayName : '').toLowerCase().search(searchText) !== -1)
+                          || (user.email ? user.email : '').toLowerCase().search(searchText) !== -1)
+              .filter((user) => (adminSwitch ? user.isAdmin : !user.isAdmin))
+              .sort((userA, userB) => {
+                if (!userA.updatedAt || !userB.updatedAt) { return true; }
+                switch (selectedIndex) {
+                  case 0: /* Newest users */
+                    if (userA.updatedAt.toDate() < userB.updatedAt.toDate()) { return 1; }
+                    return -1;
+                  case 1: /* Oldest users */
+                    if (userA.updatedAt.toDate() > userB.updatedAt.toDate()) { return 1; }
+                    return -1;
+                  case 2: /* Name - Alphabetical */
+                    if (userA.name > userB.name) { return 1; }
+                    return -1;
+                  default: /* Invalid selection */
+                    return true;
+                }
+              })
+              .map((user) => (
+                <TableRow
+                  key={user.id}
+                  className={user.isBanned ? classes.bannedRow : classes.normalRow}
+                >
+                  <TableCell component="th" scope="row">{user.displayName}</TableCell>
+                  <TableCell align="center">{user.name}</TableCell>
+                  <TableCell align="center">{user.updatedAt ? user.updatedAt.toDate().toUTCString() : 'N/A'}</TableCell>
+                  <TableCell align="center">{user.email}</TableCell>
+                  <TableCell align="center">
+                    {user.role && Array.from(user.role).map((role) => (
+                      <Typography component="div" className={classes.roleList}>
+                        <Typography componet="div" className={classes.roleListItem}>
+                          {role.toString()}
+                        </Typography>
+                      </Typography>
+                    ))}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button onClick={() => {
+                      firebase.firestore().collection('users').doc(user.id).set({
+                        ...user,
+                        isBanned: !user.isBanned,
+                      })
+                        .catch((error) => {
+                          setErrorMessage(error.message);
+                        });
+                    }}
+                    >
+                      {user.isBanned ? 'Unban' : 'Ban'}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -235,4 +140,6 @@ UsersList.propTypes = {
     }),
   ).isRequired,
   searchText: PropTypes.string.isRequired,
+  adminSwitch: PropTypes.bool.isRequired,
+  selectedIndex: PropTypes.number.isRequired,
 };
--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -7,33 +8,148 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Typography } from '@material-ui/core';
 import UsersList from './UsersList';
 
 const useStyles = makeStyles({
   searchBarRoot: {
-    // marginBottom: 20,
     padding: 5,
     display: 'flex',
-    // alignItems: 'center',
     width: '20%',
     justifyContent: 'space-between',
   },
   iconButton: {
     padding: 10,
   },
+  input: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  switchRoot: {
+    marginBottom: 20,
+    flexDirection: 'row',
+    display: 'flex',
+  },
+  switchAdmin: {
+    flex: 1,
+  },
+  dropdown: {
+    flex: 1,
+  },
+  spacing: {
+    flex: 3,
+  },
 });
 
+const sortOptions = [
+  'Newest Users',
+  'Oldest Users',
+  'Name - Alphabetical',
+];
+
+function SearchBar({ setSearchText }) {
+  const classes = useStyles();
+  return (
+    <Paper component="form" className={classes.searchBarRoot}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search Users"
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+      />
+      <IconButton type="submit" className={classes.iconButton}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
+
+function AdminSwitch({ adminSwitch, setAdminSwitch }) {
+  const classes = useStyles();
+  return (
+    <Grid component="label" container alignItems="center" spacing={1} className={classes.switchAdmin}>
+      <Grid item>Standard Users</Grid>
+      <Grid item>
+        <Switch checked={adminSwitch} onChange={(e) => { setAdminSwitch(e.target.checked); }} />
+      </Grid>
+      <Grid item>Admin Users</Grid>
+    </Grid>
+  );
+}
+
+function SortDropdown({
+  anchorEl, selectedIndex, handleClickListItem, handleClose, handleMenuItemClick,
+}) {
+  const classes = useStyles();
+  return (
+    <Typography component="div">
+      <List component="nav" className={classes.dropdown}>
+        <ListItem
+          button
+          onClick={handleClickListItem}
+        >
+          <ListItemText primary="Sort By: " secondary={sortOptions[selectedIndex]} />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {sortOptions.map((option, index) => (
+          <MenuItem
+            key={option}
+            selected={index === selectedIndex}
+            onClick={(e) => handleMenuItemClick(e, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Typography>
+  );
+}
+
 export default function UsersPage() {
+  const classes = useStyles();
   const [value, loading, error] = useCollection(
     firebase.firestore().collection('users'),
   );
-
   const [users, setUsers] = useState([]);
-
-  const classes = useStyles();
 
   /* Search bar text state */
   const [searchText, setSearchText] = useState(null);
+
+  /* Standard user to admin switch state */
+  const [adminSwitch, setAdminSwitch] = useState(false);
+
+  /* Sort dropdown hooks */
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  /* Sort dropdown functions */
+  const handleClickListItem = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleMenuItemClick = (_, i) => {
+    setSelectedIndex(i);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     if (value) {
@@ -44,22 +160,43 @@ export default function UsersPage() {
   return (
     <div>
       <h1>Users List</h1>
-      <Paper component="form" className={classes.searchBarRoot}>
-        <InputBase
-          className={classes.input}
-          placeholder="Search Users"
-          inputProps={{ 'aria-label': 'search users' }}
-          onChange={(e) => {
-            setSearchText(e.target.value);
-          }}
+      <SearchBar setSearchText={setSearchText} classes={classes} />
+      <Typography component="div" className={classes.switchRoot}>
+        <AdminSwitch adminSwitch={adminSwitch} setAdminSwitch={setAdminSwitch} />
+        <Typography component="div" className={classes.spacing} />
+        <SortDropdown
+          anchorEl={anchorEl}
+          selectedIndex={selectedIndex}
+          handleClickListItem={handleClickListItem}
+          handleClose={handleClose}
+          handleMenuItemClick={handleMenuItemClick}
         />
-        <IconButton type="submit" className={classes.iconButton} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
+      </Typography>
       {loading && <p>loading...</p>}
       {error && <p>{error.message}</p>}
-      <UsersList users={users} searchText={searchText} />
+      <UsersList
+        users={users}
+        searchText={searchText}
+        adminSwitch={adminSwitch}
+        selectedIndex={selectedIndex}
+      />
     </div>
   );
 }
+
+SearchBar.propTypes = {
+  setSearchText: PropTypes.func.isRequired,
+};
+
+AdminSwitch.propTypes = {
+  adminSwitch: PropTypes.bool.isRequired,
+  setAdminSwitch: PropTypes.func.isRequired,
+};
+
+SortDropdown.propTypes = {
+  anchorEl: PropTypes.element.isRequired,
+  selectedIndex: PropTypes.number.isRequired,
+  handleClickListItem: PropTypes.func.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleMenuItemClick: PropTypes.func.isRequired,
+};
