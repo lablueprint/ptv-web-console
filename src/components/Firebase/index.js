76d9db9f9792f28a0b932b11e44e43e07c57d60e
Vincent Cheung <vfhcheung@gmail.com>
2020-05-18 20:44:43 -0700

    Remove unused firebase code

--- src/components/Firebase/index.js
+++
@@ -1,7 +0,0 @@
-import FirebaseContext, { withFirebase } from './context';
-import Firebase from './firebase';
-import DocumentDeleteButton from './DocumentDeleteButton';
-
-export default Firebase;
-
-export { FirebaseContext, withFirebase, DocumentDeleteButton };
