0bbc71de8177e10e096d8e8f3b5d664fc1a43720
Vincent Cheung <vfhcheung@gmail.com>
2020-05-18 20:44:43 -0700

    Remove unused firebase code

--- src/components/Resources/Resource/ResourcesList.js
+++ src/components/Resources/Resource/ResourcesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { DocumentDeleteButton } from '../../Firebase';
+import DocumentDeleteButton from './DocumentDeleteButton';
 
 export default function ResourcesList({ resources, categoryId }) {
   const resourceItems = resources.map((resource) => (
