e447e3557c48af4999ae6c550c667c13eabd24e5
Bill Liu <bill.liu.school@gmail.com>
2020-05-22 23:52:07 -0400

    Ported over search bar functionality to ListView

--- src/components/ListView/ListView.js
+++ src/components/ListView/ListView.js
@@ -3,6 +3,7 @@ import IconButton from '@material-ui/core/IconButton';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,6 +11,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import SearchIcon from '@material-ui/icons/Search';
 import PropTypes from 'prop-types';
 import React, { useCallback, useState } from 'react';
 
@@ -20,7 +22,41 @@ const useStyles = makeStyles({
   container: {
     maxHeight: 440,
   },
+  searchBarRoot: {
+    marginBottom: 20,
+    padding: 5,
+    display: 'flex',
+    width: '40%',
+    justifyContent: 'space-between',
+    borderRadius: 40,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  input: {
+    marginLeft: 10,
+    flex: 1,
+  },
 });
+
+function SearchBar({ setSearchText, setPage }) {
+  const classes = useStyles();
+  return (
+    <Paper component="form" className={classes.searchBarRoot}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search Here"
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          setPage(0);
+        }}
+      />
+      <IconButton type="submit" className={classes.iconButton}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
 
 export default function ListView({
   rows, loading, errorMessage, columns, actionButtons,
@@ -28,6 +64,9 @@ export default function ListView({
   const classes = useStyles();
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  /* Search bar text state */
+  const [searchText, setSearchText] = useState(null);
 
   const handleChangePage = useCallback((event, newPage) => {
     event.preventDefault();
@@ -42,6 +81,7 @@ export default function ListView({
 
   return (
     <Paper className={classes.root} elevation={0}>
+      <SearchBar setSearchText={setSearchText} setPage={setPage} classes={classes} />
       {loading && <LinearProgress />}
       {errorMessage && <p>{errorMessage}</p>}
       <TableContainer>
@@ -60,28 +100,35 @@ export default function ListView({
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                {[columns].map((columnsList) => (
-                  columnsList.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.label}>
-                        {column.format ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })))}
-                <TableCell>
-                  {actionButtons.map(({ Icon, color }, i) => (
+            {rows.filter((row) => !searchText
+            || ([columns].map((columnsList) => (
+              columnsList.map((column) => {
+                const value = row[column.id] ? row[column.id] : '';
+                return value.toString().toLowerCase().search(searchText) !== -1;
+              })
+            ))).filter((nest) => nest.filter((item) => item).length !== 0).length !== 0)
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  {[columns].map((columnsList) => (
+                    columnsList.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.label}>
+                          {column.format ? column.format(value) : value}
+                        </TableCell>
+                      );
+                    })))}
+                  <TableCell>
+                    {actionButtons.map(({ Icon, color }, i) => (
                     // Array will never change; index okay to use as key
                     // eslint-disable-next-line react/no-array-index-key
-                    <IconButton key={i}>
-                      <Icon style={{ color }} />
-                    </IconButton>
-                  ))}
-                </TableCell>
-              </TableRow>
-            ))}
+                      <IconButton key={i}>
+                        <Icon style={{ color }} />
+                      </IconButton>
+                    ))}
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
@@ -113,4 +160,9 @@ ListView.propTypes = {
     Icon: PropTypes.elementType.isRequired,
     color: PropTypes.string,
   })).isRequired,
+};
+
+SearchBar.propTypes = {
+  setSearchText: PropTypes.func.isRequired,
+  setPage: PropTypes.func.isRequired,
 };
