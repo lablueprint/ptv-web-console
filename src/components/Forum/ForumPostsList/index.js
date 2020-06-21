d328341e6e5d41a6dfad8c5e6d4823d0adfe46e3
Bill Liu <bill.liu.school@gmail.com>
2020-06-20 23:23:06 -0400

    Removed ForumPostsListByApproval

--- src/components/Forum/ForumPostsList/index.js
+++ src/components/Forum/ForumPostsList/index.js
@@ -1,3 +1,2 @@
 export { default as RepliesChip } from './RepliesChip';
 export { default as FormattedReferencedDataField } from './FormattedReferencedDataField';
-export { default as ForumPostsListByApproval } from './ForumPostsListByApproval';
